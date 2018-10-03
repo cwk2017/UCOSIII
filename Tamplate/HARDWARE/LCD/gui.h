548176d6cee1153f031252800fb66e56ef9009ea
cwk2017 <gxbhcwk@126.com>
2018-10-03 21:45:05 +0800

    first commit

---
+++ Tamplate/HARDWARE/LCD/gui.h
@@ -0,0 +1,10 @@
+#ifndef __GUI_H
+#define __GUI_H
+#include "sys.h"
+
+void drawlcd(u16 x, u16 y, u16 xsize, u16 ysize, u16 color1, u16 color2);
+void drawlcd2(u16 x, u16 y, u16 xsize, u16 ysize);
+void drawlcd3(u16 x, u16 y, u16 xsize, u16 ysize);
+
+#endif
+
